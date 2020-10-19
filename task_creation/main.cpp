d238b60c94f8bb2f3e3f9ff58bcfb4381827e630
MCLEANS <jackagwaya@gmail.com>
2020-10-19 17:33:46 +0300

    created led tasks

--- task_creation/main.cpp
+++ task_creation/main.cpp
@@ -7,33 +7,63 @@
 #include "GPIO.h"
 
 custom_libraries::clock_config system_clock;
+custom_libraries::_GPIO green_led(GPIOD,12);
+custom_libraries::_GPIO orange_led(GPIOD,13);
+custom_libraries::_GPIO red_led(GPIOD,14);
+custom_libraries::_GPIO blue_led(GPIOD,15);
 
-extern "C" void led1_task(void* pvParameter){
+void green_led_task(void* pvParameter){
+
   while(1){
-    for(volatile int i = 0; i < 2000000; i++){}
-    GPIOD->ODR ^= (1<<12);
+    for(int i = 0; i < 900000; i++){}
+    green_led.toggle();
+
   }
 }
 
-extern "C" void led2_task(void* pvParameter){
+void orange_led_task(void* pvParameter){
+
   while(1){
-    for(volatile int i = 0; i < 2000000; i++){}
-    GPIOD->ODR ^= (1<<13);
+    for(int i = 0; i < 900000; i++){}
+    orange_led.toggle();
+  }
+}
+
+void red_led_task(void* pvParameter){
+
+  while(1){
+    for(int i = 0; i < 900000; i++){}
+    red_led.toggle();
+  }
+}
+
+void blue_led_task(void* pvParameter){
+
+  while(1){
+    for(int i = 0; i < 900000; i++){}
+    blue_led.toggle();
   }
 }
 
 int main(void) {
   
   system_clock.initialize();
-  
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-  GPIOD->MODER |= GPIO_MODER_MODER12_0;
-  GPIOD->ODR |= GPIO_ODR_ODR_12;
-  GPIOD->MODER |= GPIO_MODER_MODER13_0;
-  GPIOD->ODR |= GPIO_ODR_ODR_13;
+  //configure LEDs as output
+  green_led.pin_mode(custom_libraries::OUTPUT);
+  orange_led.pin_mode(custom_libraries::OUTPUT);
+  red_led.pin_mode(custom_libraries::OUTPUT);
+  blue_led.pin_mode(custom_libraries::OUTPUT);
 
-  xTaskCreate(led1_task,"led 1 controller",100,NULL,1,NULL);
-  xTaskCreate(led2_task,"led 2 controller",100,NULL,1,NULL);
+  green_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+
+  xTaskCreate(green_led_task,"Green led cotroller",100,NULL,1,NULL);
+  xTaskCreate(orange_led_task,"Green led cotroller",100,NULL,1,NULL);
+  xTaskCreate(red_led_task,"Green led cotroller",100,NULL,1,NULL);
+  xTaskCreate(blue_led_task,"Green led cotroller",100,NULL,1,NULL);
+  
   vTaskStartScheduler();
 
   while(1){
