5b3677fcef12c3a431b4028dffada72a56d91991
MCLEANS <jackagwaya@gmail.com>
2020-10-19 18:24:48 +0300

    Passed task parameters

--- 3_using_task_parameters/main.cpp
+++ 3_using_task_parameters/main.cpp
@@ -6,44 +6,39 @@
 
 #include "GPIO.h"
 
+/****************************************************************
+ * Here we use the task parameter to pass an Id to it and 
+ * blink a specific led with regards to the ID
+ * *************************************************************/
+
 custom_libraries::clock_config system_clock;
 custom_libraries::_GPIO green_led(GPIOD,12);
 custom_libraries::_GPIO orange_led(GPIOD,13);
 custom_libraries::_GPIO red_led(GPIOD,14);
 custom_libraries::_GPIO blue_led(GPIOD,15);
 
-void green_led_task(void* pvParameter){
+const uint8_t BLUE_LED_ID = 1;
+const uint8_t RED_LED_ID =  2;
+const uint8_t ORANGE_LED_ID = 3; 
+const uint8_t GREEN_LED_ID = 4;
 
+const uint8_t* _blue_led = &BLUE_LED_ID;
+const uint8_t* _red_led = &RED_LED_ID;
+const uint8_t* _orange_led = &ORANGE_LED_ID;
+const uint8_t* _green_led = &GREEN_LED_ID;
+
+
+void vLed_controller_task(void* pvParameter){
+  uint8_t* id = (uint8_t*) pvParameter;
   while(1){
-    for(int i = 0; i < 5000000; i++){}
-    green_led.toggle();
-
+    for(volatile int i = 0; i < 500000; i++){}
+    if(*id == 1) blue_led.toggle();
+    if(*id == 2) red_led.toggle();
+    if(*id == 3) orange_led.toggle();
+    if(*id == 4) green_led.toggle();
   }
 }
 
-void orange_led_task(void* pvParameter){
-
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
-    orange_led.toggle();
-  }
-}
-
-void red_led_task(void* pvParameter){
-
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
-    red_led.toggle();
-  }
-}
-
-void blue_led_task(void* pvParameter){
-
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
-    blue_led.toggle();
-  }
-}
 
 int main(void) {
   
@@ -59,11 +54,32 @@ int main(void) {
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
 
-  xTaskCreate(green_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(orange_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(red_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(blue_led_task,"Green led cotroller",100,NULL,1,NULL);
-  
+  //create tasks
+  xTaskCreate(vLed_controller_task,
+              "Blue led controller",
+              100,
+              (void *)_blue_led,
+              1,
+              NULL);
+  xTaskCreate(vLed_controller_task,
+              "Red led controller",
+              100,
+              (void *)_red_led,
+              1,
+              NULL);
+  xTaskCreate(vLed_controller_task,
+              "Green led controller",
+              100,
+              (void *)_green_led,
+              1,
+              NULL);
+  xTaskCreate(vLed_controller_task,
+              "orange led controller",
+              100,
+              (void *)_orange_led,
+              1,
+              NULL);
+
   vTaskStartScheduler();
 
   while(1){
