4ecbb54a9f1586884f9d349b27895b92be033bc9
MCLEANS <jackagwaya@gmail.com>
2020-10-21 15:28:56 +0300

    Implemented task priority change

--- exercise_1/main.cpp
+++ exercise_1/main.cpp
@@ -12,7 +12,44 @@ custom_libraries::_GPIO orange_led(GPIOD,13);
 custom_libraries::_GPIO red_led(GPIOD,14);
 custom_libraries::_GPIO blue_led(GPIOD,15);
 
+//Here we create task Handles
+TaskHandle_t green_led_handle;
+TaskHandle_t orange_led_handle;
+TaskHandle_t red_led_handle;
+TaskHandle_t blue_led_handle;
 
+
+void vGreen_led_controller(void* pvParameter){
+  while(1){
+    for(volatile int i = 0; i < 2000000; i++){}
+    green_led.toggle();
+    //change back green led controller priority to 1
+    vTaskPrioritySet(NULL,1);
+  }
+}
+
+void vOrange_led_controller(void* pvParameter){
+  while(1){
+    for(volatile int i = 0; i < 2000000; i++){}
+    orange_led.toggle();
+  }
+}
+
+void vRed_led_controller(void* pvParameter){
+  while(1){
+    for(volatile int i = 0; i < 2000000; i++){}
+    red_led.toggle();
+    //Change green Led controller priority
+    vTaskPrioritySet(green_led_handle,2);
+  }
+}
+
+void vBlue_led_controller(void* pvParameter){
+  while(1){
+    for(volatile int i = 0; i < 2000000; i++){}
+    blue_led.toggle();
+  }
+}
 int main(void) {
   
   system_clock.initialize();
@@ -26,8 +63,33 @@ int main(void) {
   orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
-  
-  vTaskStartScheduler();
+
+  xTaskCreate(vGreen_led_controller,
+              "Green LED Controller",
+              100,
+              NULL,
+              1,
+              &green_led_handle);
+  xTaskCreate(vOrange_led_controller,
+              "Orange LED controller",
+              100,
+              NULL,
+              1,
+              &orange_led_handle);
+  xTaskCreate(vRed_led_controller,
+              "Red LED Controller",
+              100,
+              NULL,
+              1,
+              &red_led_handle);
+  xTaskCreate(vBlue_led_controller,
+              "Blue LED Controller",
+              100,
+              NULL,
+              1,
+              &blue_led_handle);
+
+  vTaskStartScheduler(); //The freeRTOS scheduler takes over from here
 
   while(1){
 
