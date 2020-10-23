bc4b4a0ca76c10e406543770e00c26d133d95776
MCLEANS <jackagwaya@gmail.com>
2020-10-23 20:04:36 +0300

    corrected LED naming

--- task_creation/main.cpp
+++ task_creation/main.cpp
@@ -60,9 +60,9 @@ int main(void) {
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
 
   xTaskCreate(green_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(orange_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(red_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(blue_led_task,"Green led cotroller",100,NULL,1,NULL);
+  xTaskCreate(orange_led_task,"Orange led cotroller",100,NULL,1,NULL);
+  xTaskCreate(red_led_task,"Red led cotroller",100,NULL,1,NULL);
+  xTaskCreate(blue_led_task,"Blue led cotroller",100,NULL,1,NULL);
   
   vTaskStartScheduler();
 
