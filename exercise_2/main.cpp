249d8f4d23e716116d96bda573686282264e6073
MCLEANS <jackagwaya@gmail.com>
2020-10-23 19:59:16 +0300

    implemented task suspension and resumption

--- exercise_2/main.cpp
+++ exercise_2/main.cpp
@@ -16,11 +16,29 @@ custom_libraries::_GPIO blue_led(GPIOD,15);
  *  1. Task suspension ans resumption
  * *************************************************/
 
+//Create task handles
+TaskHandle_t green_led_handle;
+TaskHandle_t orange_led_handle;
+TaskHandle_t red_led_handle;
+TaskHandle_t blue_led_handle;
+
+uint8_t suspension_counter = 0;
+uint8_t resumption_counter = 0;
+bool is_suspended = false;
+
 void green_led_task(void* pvParameter){
 
   while(1){
-    vTaskDelay(pdMS_TO_TICKS(500));
     green_led.toggle();
+    if(is_suspended){
+      resumption_counter++;
+      if(resumption_counter >= 10){
+        is_suspended = false;
+        resumption_counter = 0;
+        vTaskResume(orange_led_handle);
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(500));
 
   }
 }
@@ -28,24 +46,30 @@ void green_led_task(void* pvParameter){
 void orange_led_task(void* pvParameter){
 
   while(1){
-    vTaskDelay(pdMS_TO_TICKS(500));
     orange_led.toggle();
+    vTaskDelay(pdMS_TO_TICKS(500));
+    suspension_counter ++;
+    if(suspension_counter >= 10){
+      is_suspended = true;
+      suspension_counter = 0;
+      vTaskSuspend(orange_led_handle);
+    }
   }
 }
 
 void red_led_task(void* pvParameter){
 
   while(1){
-    vTaskDelay(pdMS_TO_TICKS(500));
     red_led.toggle();
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
 void blue_led_task(void* pvParameter){
 
   while(1){
-    vTaskDelay(pdMS_TO_TICKS(500));
     blue_led.toggle();
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
@@ -63,10 +87,10 @@ int main(void) {
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
 
-  xTaskCreate(green_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(orange_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(red_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(blue_led_task,"Green led cotroller",100,NULL,1,NULL);
+  xTaskCreate(green_led_task,"Green led cotroller",100,NULL,1,&green_led_handle);
+  xTaskCreate(orange_led_task,"Orange led cotroller",100,NULL,1,&orange_led_handle);
+  xTaskCreate(red_led_task,"Red led cotroller",100,NULL,1,&red_led_handle);
+  xTaskCreate(blue_led_task,"Blue led cotroller",100,NULL,1,&blue_led_handle);
   
   vTaskStartScheduler();
 
