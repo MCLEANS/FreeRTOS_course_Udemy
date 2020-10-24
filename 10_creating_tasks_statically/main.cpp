975994a1d8ccca1d5277c317a09ae4fca831bf4f
MCLEANS <jackagwaya@gmail.com>
2020-10-24 22:43:07 +0300

    Initialized LEDs

--- 10_creating_tasks_statically/main.cpp
+++ 10_creating_tasks_statically/main.cpp
@@ -4,14 +4,28 @@
 #include <task.h>
 #include <portmacro.h>
 
+#include "GPIO.h"
+
 custom_libraries::clock_config system_clock;
 
+custom_libraries::_GPIO green_led(GPIOD,12);
+custom_libraries::_GPIO orange_led(GPIOD,13);
+custom_libraries::_GPIO red_led(GPIOD,14);
+custom_libraries::_GPIO blue_led(GPIOD,15);
 
 int main(void) {
-  
+
   system_clock.initialize();
+  green_led.pin_mode(custom_libraries::OUTPUT);
+  orange_led.pin_mode(custom_libraries::OUTPUT);
+  red_led.pin_mode(custom_libraries::OUTPUT);
+  blue_led.pin_mode(custom_libraries::OUTPUT);
 
-
+  green_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  
   while(1){
 
   }
