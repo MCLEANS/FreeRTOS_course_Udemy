428c60b99ea63c6bbd1b0d527136542a17cef0a5
MCLEANS <jackagwaya@gmail.com>
2020-10-27 17:26:46 +0300

    Added other LED tasks

--- 10_creating_tasks_statically/main.cpp
+++ 10_creating_tasks_statically/main.cpp
@@ -6,6 +6,12 @@
 
 #include "GPIO.h"
 
+/**
+ * 1. Add (#define configSUPPORT_STATIC_ALLOCATION 1) into the FreeRTOSConfig.h file
+ * 2. Create vApplicationIdleTaskMemory callback
+ * 3. Create vApplicationTimerTaskMemory callback
+ */ 
+
 custom_libraries::clock_config system_clock;
 
 custom_libraries::_GPIO green_led(GPIOD,12);
@@ -13,9 +19,24 @@ custom_libraries::_GPIO orange_led(GPIOD,13);
 custom_libraries::_GPIO red_led(GPIOD,14);
 custom_libraries::_GPIO blue_led(GPIOD,15);
 
+//Blue task memory allocation
 #define blue_led_task_STACK_SIZE 100
 StaticTask_t blue_led_task_buffer;
-StackType_t blue_led_task_STACK[100]; //StackType_t is defined as uint32_t 
+StackType_t blue_led_task_STACK[blue_led_task_STACK_SIZE]; //StackType_t is defined as uint32_t 
+
+//Red task Memory allocation
+#define red_led_task_STACK_SIZE 100
+StaticTask_t red_led_task_buffer;
+StackType_t red_led_task_STACK[red_led_task_STACK_SIZE];
+
+#define orange_led_task_STACK_SIZE 100
+StaticTask_t orange_led_task_buffer;
+StackType_t orange_led_task_STACK[orange_led_task_STACK_SIZE];
+
+#define green_led_task_STACK_SIZE 100
+StaticTask_t green_led_task_buffer;
+StackType_t green_led_task_STACK[green_led_task_STACK_SIZE];
+
 
 /**
  * Blue LED Task
@@ -28,6 +49,92 @@ void blue_led_task(void* pvParameter){
     vTaskDelay(pdMS_TO_TICKS(200));
   }
 }
+
+/**
+ * Red LED Task
+ */
+void red_led_task(void* pvParameter){
+
+  while(1){
+    red_led.toggle();
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+/**
+ * Orange LED Task
+ */
+void orange_led_task(void* pvParameter){
+
+  while(1){
+    orange_led.toggle();
+    vTaskDelay(pdMS_TO_TICKS(300));
+  }
+}
+
+/**
+ * Green LED Task
+ */
+void green_led_task(void* pvParameter){
+
+  while(1){
+    green_led.toggle();
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+int main(void) {
+
+  system_clock.initialize();
+  green_led.pin_mode(custom_libraries::OUTPUT);
+  orange_led.pin_mode(custom_libraries::OUTPUT);
+  red_led.pin_mode(custom_libraries::OUTPUT);
+  blue_led.pin_mode(custom_libraries::OUTPUT);
+
+  green_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+
+  xTaskCreateStatic(blue_led_task,
+                    "Blue Led Controller task",
+                    blue_led_task_STACK_SIZE,
+                    NULL,
+                    1,
+                    blue_led_task_STACK,
+                    &blue_led_task_buffer);
+
+  xTaskCreateStatic(red_led_task,
+                    "Red Led Controller Task",
+                    red_led_task_STACK_SIZE,
+                    NULL,
+                    1,
+                    red_led_task_STACK,
+                    &red_led_task_buffer);
+
+  xTaskCreateStatic(orange_led_task,
+                    "Orange Led Controller Task",
+                    orange_led_task_STACK_SIZE,
+                    NULL,
+                    1,
+                    orange_led_task_STACK,
+                    &orange_led_task_buffer);
+
+  xTaskCreateStatic(green_led_task,
+                    "Green Led Controller task",
+                    green_led_task_STACK_SIZE,
+                    NULL,
+                    1,
+                    green_led_task_STACK,
+                    &green_led_task_buffer);
+
+  vTaskStartScheduler();
+  
+  while(1){
+
+  }
+}
+
 
 /**
  * Timer Task Memory Callback
@@ -55,33 +162,4 @@ extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffe
     *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
     *ppxIdleTaskStackBuffer = uxIdleTaskStack;
     *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-
-
-
-int main(void) {
-
-  system_clock.initialize();
-  green_led.pin_mode(custom_libraries::OUTPUT);
-  orange_led.pin_mode(custom_libraries::OUTPUT);
-  red_led.pin_mode(custom_libraries::OUTPUT);
-  blue_led.pin_mode(custom_libraries::OUTPUT);
-
-  green_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
-  orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
-  red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
-  blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
-
-  xTaskCreateStatic(blue_led_task,
-                    "Blue Led Controller task",
-                    blue_led_task_STACK_SIZE,
-                    NULL,
-                    1,
-                    blue_led_task_STACK,
-                    &blue_led_task_buffer);
-  vTaskStartScheduler();
-  
-  while(1){
-
-  }
 }
