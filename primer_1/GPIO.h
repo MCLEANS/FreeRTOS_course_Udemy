25f659b6cf66b4d9c3bc79a4211421fcb07eb355
MCLEANS <jackagwaya@gmail.com>
2020-12-15 17:07:50 +0300

    rectified file eror

--- primer_1/GPIO.h
+++ primer_1/GPIO.h
@@ -1,117 +1,63 @@
 /*
- * GPIO.cpp
+ * GPIO.h
  *
  *  Created on: May 4, 2020
  *      Author: MCLEANS
  */
 
-#include "GPIO.h"
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "stm32f4xx.h"
 
 namespace custom_libraries {
 
-_GPIO::_GPIO(GPIO_TypeDef *GPIO,uint8_t PIN):GPIO(GPIO),
-											PIN(PIN),
-											mode(OUTPUT),
-											type(OPEN_DRAIN),
-											speed(LOW),
-											state(FLOATING){
-	// TODO Auto-generated constructor stub
-	//Enable GPIO RCC
-	if(GPIO == GPIOA) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	if(GPIO == GPIOB) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-	if(GPIO == GPIOC) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-	if(GPIO == GPIOD) RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-	if(GPIO == GPIOE) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
-	if(GPIO == GPIOF) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN;
-	if(GPIO == GPIOG) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
-	if(GPIO == GPIOH) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN;
-	if(GPIO == GPIOI) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOIEN;
+enum Mode{
+	INPUT,
+	OUTPUT
+};
 
-}
+enum OUTPUT_Type{
+	PUSH_PULL,
+	OPEN_DRAIN
+};
 
-void _GPIO::pin_mode(Mode mode){
-	this->mode = mode;
+enum OUTPUT_Speed{
+	LOW,
+	MEDUIM,
+	HIGH,
+	VERY_HIGH
+};
 
-	if(mode == INPUT){
-		GPIO->MODER &= ~(1<<(PIN*2));
-		GPIO->MODER &= ~(1<<((PIN*2)+1));
-	}
-	else if(mode == OUTPUT){
-		GPIO->MODER |= (1<<(PIN*2));
-		GPIO->MODER &= ~(1<<((PIN*2)+1));
-	}
-}
+enum State{
+	FLOATING,
+	PULL_UP,
+	PULL_DOWN
+};
 
-void _GPIO::output_settings(OUTPUT_Type type, OUTPUT_Speed speed){
-	this->type = type;
-	this->speed = speed;
 
-	if(type == OPEN_DRAIN){
-		GPIO->OTYPER |= (1<<PIN);
-	}
-	else if(type == PUSH_PULL){
-		GPIO->OTYPER &= ~(1<<PIN);
-	}
-	if(speed == LOW){
-		GPIO->OSPEEDR &= ~(1<<(PIN*2));
-		GPIO->OSPEEDR &= ~(1<<((PIN*2)+1));
-	}
-	else if(speed == MEDUIM){
-		GPIO->OSPEEDR |= (1<<(PIN*2));
-		GPIO->OSPEEDR &= ~(1<<((PIN*2)+1));
-	}
-	else if(speed == HIGH){
-		GPIO->OSPEEDR &= ~(1<<(PIN*2));
-		GPIO->OSPEEDR |= (1<<((PIN*2)+1));
-	}
-	else if(speed == VERY_HIGH){
-		GPIO->OSPEEDR |= (1<<(PIN*2));
-		GPIO->OSPEEDR |= (1<<((PIN*2)+1));
-	}
+class _GPIO {
+private:
+	GPIO_TypeDef *GPIO;
+	uint8_t PIN;
+	Mode mode;
+	OUTPUT_Type type;
+	OUTPUT_Speed speed;
+	State state;
+private:
+public:
+public:
+	_GPIO(GPIO_TypeDef *GPIO,uint8_t PIN);
+	void pin_mode(Mode mode);
+	void output_settings(OUTPUT_Type type, OUTPUT_Speed speed);
+	void input_state(State state);
+	void digital_write(bool value);
+	bool digital_read(void) const;
+	void toggle();
 
-}
-
-void _GPIO::input_state(State state){
-	this->state = state;
-
-	if(state == FLOATING){
-		GPIO->PUPDR &= ~(1<<(PIN*2));
-		GPIO->PUPDR &= ~(1<<((PIN*2)+1));
-	}
-	else if(state == PULL_UP){
-		GPIO->PUPDR |= (1<<(PIN*2));
-		GPIO->PUPDR &= ~(1<<((PIN*2)+1));
-
-	}
-	else if(state == PULL_DOWN){
-		GPIO->PUPDR &= ~(1<<(PIN*2));
-		GPIO->PUPDR |= (1<<((PIN*2)+1));
-	}
-}
-
-void _GPIO::digital_write(bool value){
-	if(value){
-		GPIO->ODR |= (1<<PIN);
-	}
-	if(!value){
-		GPIO->ODR &= ~(1<<PIN);
-	}
-}
-
-bool _GPIO::digital_read(void)const{
-	if(GPIO->IDR & (1<<PIN)){
-		return true;
-	}
-
-	return false;
-}
-	
-void _GPIO::toggle(){
-	GPIO->ODR ^= (1 << PIN);
-}
-
-_GPIO::~_GPIO() {
-	// TODO Auto-generated destructor stub
-}
+	~_GPIO();
+};
 
 } /* namespace custom_libraries */
+
+#endif /* GPIO_H_ */
