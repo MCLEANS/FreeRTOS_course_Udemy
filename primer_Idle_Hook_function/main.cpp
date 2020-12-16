8884e43b6b034b97196a5deef8a53b1c70b4f6af
MCLEANS <jackagwaya@gmail.com>
2020-12-16 19:49:37 +0300

    Activated and defined idle task function

--- primer_Idle_Hook_function/main.cpp
+++ primer_Idle_Hook_function/main.cpp
@@ -8,22 +8,27 @@
  * 1. An idle task will always have the lowest priority (priority 0)
  * 2. It will only run when there are no higher priority tasks runining (tasks such as memory cleanup can be runnned here)
  * 3. In order to use the idle hook function you have to enable it in the FreeRTOS config file.
+ * 4. To use the idle hook we use the callback vApplicationIdleHook() 
  */
 
 custom_libraries::clock_config system_clock;
 
-extern "C" void led1_task(void* pvParameter){
+void led1_task(void* pvParameter){
   while(1){
     for(volatile int i = 0; i < 2000000; i++){}
     GPIOD->ODR ^= (1<<12);
   }
 }
 
-extern "C" void led2_task(void* pvParameter){
+void led2_task(void* pvParameter){
   while(1){
-    for(volatile int i = 0; i < 2000000; i++){}
     GPIOD->ODR ^= (1<<13);
+    vTaskDelay(pdMS_TO_TICKS(250));
   }
+}
+
+extern "C" void vApplicationIdleHook(void){
+
 }
 
 int main(void) {
@@ -37,7 +42,6 @@ int main(void) {
   GPIOD->ODR |= GPIO_ODR_ODR_13;
 
   xTaskCreate(led1_task,"led 1 controller",100,NULL,1,NULL);
-  xTaskCreate(led2_task,"led 2 controller",100,NULL,1,NULL);
   vTaskStartScheduler();
 
   while(1){
