4211c07718340529c2ad18a80976c2145b256355
MCLEANS <jackagwaya@gmail.com>
2020-12-17 10:06:41 +0300

    configured freeRTOS as completely peemptive

--- 14_configuring_scheduler_Preemptive/FreeRTOS/Source/include/FreeRTOSConfig.h
+++ 14_configuring_scheduler_Preemptive/FreeRTOS/Source/include/FreeRTOSConfig.h
@@ -47,6 +47,7 @@
 	extern uint32_t SystemCoreClock;
 #endif
 
+#define configUSE_TIME_SLICING			0
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
