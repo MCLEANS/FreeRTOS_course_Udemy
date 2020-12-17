3beafb256dad2e952185465475df8a3b1ce5f690
MCLEANS <jackagwaya@gmail.com>
2020-12-17 09:28:28 +0300

    Implemented co-operative idle task and disabled idle task yielding

--- 13_cooperative_Idle_Task/main.cpp
+++ 13_cooperative_Idle_Task/main.cpp
@@ -11,6 +11,12 @@
  * 2. To use the Idle task we need to use the callback known as vApplicationIdleHook
  * 3. The idle task will run whenever there is no task to be run.
  * 4. the idle task has the lowest task priority.
+ * 
+ * 5. Cooperative idle task is when we have another task with a priority same as that of the idle task (0)
+ *    In such a scenerio the Scheduler tries to implement some sort of time sharing between the tasks.
+ * 6. When "idle task should yield is set to 1" the idle task only executes once and then exits,
+ *    it does not use all the time allocated to it. When this setting is disabled then the idle task will run repeteadly until
+ *    all the time allocated to it is depleted.
  */ 
 
 custom_libraries::clock_config system_clock;
@@ -26,12 +32,13 @@ void red_led_task(void* pvParameter){
   while(1){
     red_led.toggle();
     red_counter++;
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
 
 extern "C" void vApplicationIdleHook(){
     green_led.toggle();
+    for(volatile int i = 0; i < 5000000; i++){}
 }
 
 int main(void) {
@@ -48,7 +55,7 @@ int main(void) {
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
 
-  xTaskCreate(red_led_task,"Red led cotroller",100,NULL,1,NULL);
+  xTaskCreate(red_led_task,"Red led cotroller",100,NULL,0,NULL);
   
   vTaskStartScheduler();
 
