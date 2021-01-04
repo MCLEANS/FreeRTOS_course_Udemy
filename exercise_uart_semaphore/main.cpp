8865ab911273fd67d10c4d43adc6e219faaca8aa
MCLEANS <jackagwaya@gmail.com>
2021-01-04 16:25:45 +0300

    Implemented tasks

--- exercise_uart_semaphore/main.cpp
+++ exercise_uart_semaphore/main.cpp
@@ -34,13 +34,51 @@ custom_libraries::USART serial(USART1,
                               SERIAL_TX,
                               BAUDRATE);
 
+/**
+ * Task handles
+ */
+TaskHandle_t greetings_task;
+TaskHandle_t notification_task;
+
+void send_greeting(void* pvParameter){
+  char greeting[] = "Hello World";
+  while(1){
+    serial.println(greeting);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+void send_notification(void* pvParameter){
+  char notification[] = "Sending notification";
+  while(1){
+    serial.println(notification);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
 
 int main(void) {
   
   system_clock.initialize();
   serial.initialize();
 
-  
+  /**
+   * Create project tasks 
+   */
+  xTaskCreate(send_greeting,
+              "Greetings task",
+              100,
+              NULL,
+              1,
+              &greetings_task);
+
+  xTaskCreate(send_notification,
+              "Notification task",
+              100,
+              NULL,
+              1,
+              &notification_task);
+
+  vTaskStartScheduler();
 
   while(1){
 
