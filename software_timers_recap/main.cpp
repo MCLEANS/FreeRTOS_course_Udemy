f2453aae1b233f3a5a00cfae611ac300f12a60a4
MCLEANS <jackagwaya@gmail.com>
2021-05-12 22:27:17 +0300

    Initial commit

---
+++ software_timers_recap/main.cpp
@@ -0,0 +1,32 @@
+#include "stm32f4xx.h"
+#include "clockconfig.h"
+#include <FreeRTOS.h>
+#include <task.h>
+#include <portmacro.h>
+
+#include "GPIO.h"
+
+custom_libraries::clock_config system_clock;
+custom_libraries::_GPIO green_led(GPIOD,12);
+custom_libraries::_GPIO orange_led(GPIOD,13);
+custom_libraries::_GPIO red_led(GPIOD,14);
+custom_libraries::_GPIO blue_led(GPIOD,15);
+
+int main(void) {
+  
+  system_clock.initialize();
+  //configure LEDs as output
+  green_led.pin_mode(custom_libraries::OUTPUT);
+  orange_led.pin_mode(custom_libraries::OUTPUT);
+  red_led.pin_mode(custom_libraries::OUTPUT);
+  blue_led.pin_mode(custom_libraries::OUTPUT);
+
+  green_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+  blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+
+  while(1){
+
+  }
+}
