d300cf7f8b5efc420eb947a0e940d5733229ee50
MCLEANS <jackagwaya@gmail.com>
2021-05-12 22:32:12 +0300

    Added red and blue blink

--- software_timers_recap/main.cpp
+++ software_timers_recap/main.cpp
@@ -12,6 +12,27 @@ custom_libraries::_GPIO orange_led(GPIOD,13);
 custom_libraries::_GPIO red_led(GPIOD,14);
 custom_libraries::_GPIO blue_led(GPIOD,15);
 
+/**
+ * Task handles
+ */
+TaskHandle_t red_led_task;
+TaskHandle_t blue_led_task;
+
+/**
+ * System tasks
+ */
+void red_blink(void* pvParam){
+  while(1){
+
+  }
+}
+
+void blue_blink(void* pvParam){
+  while(1){
+    
+  }
+}
+
 int main(void) {
   
   system_clock.initialize();
@@ -25,6 +46,20 @@ int main(void) {
   orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+
+  /* Create system tasks */
+  xTaskCreate(red_blink,
+              "Red Led controller",
+              100,
+              NULL,
+              1,
+              &red_led_task);
+  xTaskCreate(blue_blink,
+              "Blue Led controller",
+              100,
+              NULL,
+              1,
+              &blue_led_task);
 
   while(1){
 
