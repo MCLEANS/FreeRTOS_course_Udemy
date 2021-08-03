7d9ed5f465edbf084599230e8de9d2db9777c55c
MCLEANS <jackagwaya@gmail.com>
2021-08-03 21:48:07 +0300

    tested software timer

--- software_timers_primer/main.cpp
+++ software_timers_primer/main.cpp
@@ -5,6 +5,7 @@
 #include <portmacro.h>
 
 #include "GPIO.h"
+#include <timers.h>
 
 custom_libraries::clock_config system_clock;
 custom_libraries::_GPIO green_led(GPIOD,12);
@@ -12,35 +13,18 @@ custom_libraries::_GPIO orange_led(GPIOD,13);
 custom_libraries::_GPIO red_led(GPIOD,14);
 custom_libraries::_GPIO blue_led(GPIOD,15);
 
-void green_led_task(void* pvParameter){
+/**
+ * Timer handles
+ */
+TimerHandle_t red_timer_handle;
+TimerHandle_t blue_timer_handle;
 
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
-    green_led.toggle();
-
+void toggle_timer_callback(TimerHandle_t xTimer){
+  uint32_t timer_id = (uint32_t) pvTimerGetTimerID(xTimer);
+  if(timer_id == 0){
+    red_led.toggle();
   }
-}
-
-void orange_led_task(void* pvParameter){
-
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
-    orange_led.toggle();
-  }
-}
-
-void red_led_task(void* pvParameter){
-
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
-	red_led.toggle();
-  }
-}
-
-void blue_led_task(void* pvParameter){
-
-  while(1){
-    for(int i = 0; i < 5000000; i++){}
+  else if(timer_id == 1){
     blue_led.toggle();
   }
 }
@@ -59,10 +43,26 @@ int main(void) {
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
 
-  xTaskCreate(green_led_task,"Green led cotroller",100,NULL,1,NULL);
-  xTaskCreate(orange_led_task,"Orange led cotroller",100,NULL,1,NULL);
-  xTaskCreate(red_led_task,"Red led cotroller",100,NULL,1,NULL);
-  xTaskCreate(blue_led_task,"Blue led cotroller",100,NULL,1,NULL);
+  /**
+   * Create software timer
+   */
+  red_timer_handle = xTimerCreate("Red LED timer",
+                                  pdMS_TO_TICKS(500),
+                                  pdTRUE,
+                                  (void*)0,
+                                  toggle_timer_callback);
+  if(red_timer_handle != NULL){
+    xTimerStart(red_timer_handle,0);
+  }
+
+  blue_timer_handle = xTimerCreate("Blue LED timer",
+                                    pdMS_TO_TICKS(200),
+                                    pdTRUE,
+                                    (void*)1,
+                                    toggle_timer_callback);
+  if(blue_timer_handle != NULL){
+    xTimerStart(blue_timer_handle,0);
+  }
   
   vTaskStartScheduler();
 
