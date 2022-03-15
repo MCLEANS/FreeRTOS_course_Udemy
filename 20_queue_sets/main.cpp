438ad1b2330dd93664355142ea407fa27dcc7fa7
MCLEANS <jackagwaya@gmail.com>
2022-03-15 12:31:56 +0300

    create tasks

--- 20_queue_sets/main.cpp
+++ 20_queue_sets/main.cpp
@@ -8,11 +8,20 @@
 #include "LIS3DH.h"
 #include "EXTI.h"
 
+#define INTERRUPT_PORT GPIOA
+#define INTERRUPT_PIN 4
+
 custom_libraries::clock_config system_clock;
 custom_libraries::_GPIO green_led(GPIOD,12);
 custom_libraries::_GPIO orange_led(GPIOD,13);
 custom_libraries::_GPIO red_led(GPIOD,14);
 custom_libraries::_GPIO blue_led(GPIOD,15);
+
+/* Create interrupt  edge type */
+custom_libraries::edge response_edge = custom_libraries::RISING;
+
+/* Create interrupt object */
+custom_libraries::_EXTI halt_button(INTERRUPT_PORT,INTERRUPT_PIN,response_edge);
 
 /* LIS3DH pin definition */
 #define SCK_PIN 5
@@ -31,13 +40,30 @@ custom_libraries::LIS3DH accelerometer(SPI1,
                                       CS_PORT,
                                       CS_PIN);
 
+TaskHandle_t sensor_read_handle;
+
+/* External Interrupts ISR */
+void EXTI4_IRQhandler(void){
+  /* verify interrupt, reset flag and give semaphore */
+
+}
+
+
 
 int main(void) {
   
   system_clock.initialize();
 
+  /* Initialize interrupt pin */
+  halt_button.initialize();
+
+  /* Set interrupt vector and enable */
+  NVIC_SetPriority(EXTI4_IRQn, 0x05);
+  NVIC_EnableIRQ(EXTI4_IRQn);
+
   /* Initialize accelerometer */
   accelerometer.initialize();
+
   //configure LEDs as output
   green_led.pin_mode(custom_libraries::OUTPUT);
   orange_led.pin_mode(custom_libraries::OUTPUT);
@@ -48,6 +74,9 @@ int main(void) {
   orange_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   red_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
   blue_led.output_settings(custom_libraries::PUSH_PULL,custom_libraries::VERY_HIGH);
+
+  xTaskCreate(sensor_read,
+              )
   
   vTaskStartScheduler();
 
